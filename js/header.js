3730a4f012b26287d7987bee3f49a49931553d9d
Gleb <shaevgleb@mail.ru>
2022-08-28 12:31:28 +0300

    changes

--- js/header.js
+++ js/header.js
@@ -4,3 +4,39 @@ $(document).ready(function() {
         $('body').toggleClass('lock');
     });
 });
+
+
+const links1 = document.querySelector('.header__link1');
+const links2 = document.querySelector('.header__link2');
+const links3 = document.querySelector('.header__link3');
+const links4 = document.querySelector('.header__link4');
+const links5 = document.querySelector('.header__link5');
+const headerBurger = document.querySelector('.header__burger');
+const headerMenu = document.querySelector('.header__menu');
+const dopSupstar = document.querySelector('.dop__supstar');
+
+links1.addEventListener("click", () => {
+    headerBurger.classList.remove('active');
+    headerMenu.classList.remove('active');
+    dopSupstar.classList.remove('active');
+});
+links2.addEventListener("click", () => {
+    headerBurger.classList.remove('active');
+    headerMenu.classList.remove('active');
+    dopSupstar.classList.remove('active');
+});
+links3.addEventListener("click", () => {
+    headerBurger.classList.remove('active');
+    headerMenu.classList.remove('active');
+    dopSupstar.classList.remove('active');
+});
+links4.addEventListener("click", () => {
+    headerBurger.classList.remove('active');
+    headerMenu.classList.remove('active');
+    dopSupstar.classList.remove('active');
+});
+links5.addEventListener("click", () => {
+    headerBurger.classList.remove('active');
+    headerMenu.classList.remove('active');
+    dopSupstar.classList.remove('active');
+});
